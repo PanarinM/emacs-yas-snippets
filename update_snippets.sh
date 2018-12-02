df552706a19319f5dfaaeb8e8eb2b73fc048666d
mpanarin <m.panarin@mobilunity.com>
2018-12-02 18:20:44 +0200

    [ADD] update_snippets script

---
+++ update_snippets.sh
@@ -0,0 +1,3 @@
+ln -sf ~/emacs-yas-snippets/python-mode ~/.emacs.d/private/snippets/python-mode
+ln -sf ~/emacs-yas-snippets/web-mode ~/.emacs.d/private/snippets/web-mode
+ln -sf ~/emacs-yas-snippets/elixir-mode ~/.emacs.d/private/snippets/elixir-mode
